68568ef04913c90e9a80aafd8f43c6634cf26990
Wolfgang Ulmer <wulmer@web.de>
2016-12-30 00:44:18 +0100

    Allow button A+B to start the wheel

--- wheeloffortune.py
+++ wheeloffortune.py
@@ -2,11 +2,10 @@ from microbit import *
 import random
 
 while True:
-    if button_a.was_pressed():
+    if button_a.was_pressed() or button_b.was_pressed():
         for delay in (10, 20, 50, 100):
             display.show(Image.ALL_CLOCKS, delay)
         number = random.randint(1, 10)
         display.show(str(number))
     else:
-        sleep(100)
-        
+        sleep(500)
