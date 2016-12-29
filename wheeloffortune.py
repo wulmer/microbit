9079470c831781ae5f720a9e92ffb93c9e83bfb8
Wolfgang Ulmer <wulmer@web.de>
2016-12-29 23:18:05 +0100

    Add wheel of fortune with numbers 1-10

---
+++ wheeloffortune.py
@@ -0,0 +1,12 @@
+from microbit import *
+import random
+
+while True:
+    if button_a.was_pressed():
+        for delay in (10, 20, 50, 100):
+            display.show(Image.ALL_CLOCKS, delay)
+        number = random.randint(1, 10)
+        display.show(str(number))
+    else:
+        sleep(100)
+        
